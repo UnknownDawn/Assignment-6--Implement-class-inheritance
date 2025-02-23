44ef567529e967591d965226f7764b8216cb65de
UnknownDawn <jbklippst@gmail.com>
2025-02-22 22:50:15 -0500

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,55 @@
+
+
+public class Assignment{
+    
+/**
+ * Justin Klippstein
+ * Assignment 5 / 6
+ * 
+ * 
+ * Please be lenient, my sister went to an urgent care today and got 7 stitches, If anything is left unfinished
+ * I will make sure to fix it as soon as I get back.
+ */
+
+
+
+
+    public static void main(String[] args) throws Exception {
+
+
+    }
+
+
+public class Animal {
+    
+    public int age;
+    public String gender;
+
+public static void isMammal() {
+    System.out.println("Is Mamal function in Animal class");
+}
+public static void mate() {
+    System.out.println("Animal Mate In Animal Class");
+}
+
+}
+
+
+public class Fish extends Animal {
+
+    private  int sizeInFeet;
+
+    private static void canEat() {
+
+        System.out.println("Fish Eat in Fish Class");
+    }
+}
+
+
+public class Zebra extends Animal {
+
+    public bool is_wild;
+
+    }
+
+}
